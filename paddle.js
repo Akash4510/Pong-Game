3bb12e614f42afb53231f592667fdb7691edd015
Akash <test.email.cr7@gmail.com>
2021-12-05 01:22:01 +0530

    Added paddle movement for the computer AI to play the game and also added the functionality to update and reset paddle

---
+++ paddle.js
@@ -0,0 +1,27 @@
+const SPEED = 0.02;
+
+export default class Paddle {
+    constructor(paddleElem) {
+        this.paddleElem = paddleElem;
+        this.reset();
+    }
+
+    get position() {
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--position"));
+    }
+
+    set position(value) {
+        this.paddleElem.style.setProperty("--position", value)
+    }
+
+    update(delta, ballHeight) {
+        // This single line will make the computer unbeatable.
+        // this.position = ballHeight;
+
+        this.position += SPEED * delta * (ballHeight - this.position);
+    }
+
+    reset() {
+        this.position = 50;
+    }
+}
