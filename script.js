e0a03b24a83f79b6b893338c8deb1397c65f710c
Akash <test.email.cr7@gmail.com>
2021-12-05 02:08:44 +0530

    Fixed major bug (the game stops when the ball hit the left or the right boundary)

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const ball = new Ball(document.getElementById("ball"));
 const playerPaddle = new Paddle(document.getElementById("player-paddle"));
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
-const playerScoreElem = document.getElementById(".player-score");
-const computerScoreElem = document.getElementById(".computer-score");
+const playerScoreElem = document.getElementById("player-score");
+const computerScoreElem = document.getElementById("computer-score");
 
 let lastTime;
 function update(time) {
