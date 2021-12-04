b2c8f5a2604677e391581695d3a76179beb21b6c
Akash <test.email.cr7@gmail.com>
2021-12-05 01:04:36 +0530

    Added all the ball functionality

--- ball.js
+++ ball.js
@@ -1,9 +1,14 @@
 const INITIAL_VELOCITY = 0.025;
+const VELOCITY_INCREASE = 0.00001;
 
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem;
         this.reset();
+    }
+
+    rect() {
+        return this.ballElem.getBoundingClientRect();
     }
 
     get x() {
@@ -23,22 +28,41 @@ export default class Ball {
     }
 
     reset() {
-        this.x = 50;
-        this.y = 50;
-        this.direction = { x: 0 }
+        this.x = 50;  // Setting the initial value of x to be at center
+        this.y = 50;  // Setting the initial value of y to be at center
+
+        this.direction = { x: 0, y: 0 }  // Setting up a direction for the ball to move
         while (Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= .9) {
+            // Getting a random direction for the ball to move
             const heading = randomNumberBetween(0, 2 * Math.PI);
+            // Setting the heading of the ball to move in that direction
             this.direction = {
                 x: Math.cos(heading), y: Math.sin(heading)
             }
         }
-        console.log(this.direction);
+        // Setting up the velocity to start at initail velocity
         this.velocity = INITIAL_VELOCITY;
     }
 
     update(delta) {
+        // Incrementing the x and y position of the ball 
+        // Multiplying the value with the delta so that the ball will appear to move in the same speed even in the frame drops
         this.x += this.direction.x * this.velocity * delta;
         this.y += this.direction.y * this.velocity * delta;
+
+        // Increasing the velocity with time
+        this.velocity += VELOCITY_INCREASE * delta;
+
+        // Reversing the y direction of the ball if it hits the top side or the bottom side of the screen. 
+        const rect = this.rect();
+        if (rect.bottom >= window.innerHeight || rect.top <= 0) {
+            this.direction.y *= -1;
+        }
+
+        // Reversing the x direction of the ball if it hits the left side or the right side of the screen.
+        if (rect.right >= window.innerWidth || rect.left <= 0) {
+            this.direction.x *= -1;
+        }
     }
 }
 
