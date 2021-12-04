633a98c562a11c2313e0a57c573c4238f592c8de
Akash <test.email.cr7@gmail.com>
2021-12-05 00:40:36 +0530

    Added update and reset function for the ball and added the functionality so the ball will start moving in a random direction when the reset function is called

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert("Working properly");
+import Ball from "./ball.js";
+
+const ball = new Ball(document.getElementById("ball"));
+
+let lastTime;
+function update(time) {
+
+    if (lastTime != null) {
+        const delta = time - lastTime;
+        ball.update(delta);
+    }
+
+    lastTime = time;
+    // It updates the screen every time an object is moved.
+    window.requestAnimationFrame(update);
+}
+
+// This will create an infinite loop
+window.requestAnimationFrame(update);
