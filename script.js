f312858922dbc00749c849b1c236e48534b7ec97
Akash <test.email.cr7@gmail.com>
2021-12-05 01:23:12 +0530

    Script without the proper game logic

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 import Ball from "./ball.js";
+import Paddle from "./paddle.js";
 
 const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
 let lastTime;
 function update(time) {
 
     if (lastTime != null) {
         const delta = time - lastTime;
-        ball.update(delta);
+        // ball.update(delta);
+        computerPaddle.update(delta, ball.y);
     }
 
     lastTime = time;
     // It updates the screen every time an object is moved.
     window.requestAnimationFrame(update);
 }
+
+document.addEventListener("mousemove", e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100;
+})
 
 // This will create an infinite loop
 window.requestAnimationFrame(update);
